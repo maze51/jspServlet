5283ee6143b146c81ca9275d5118bb6fb2a3dfda
YC <mazexit@gmail.com>
2019-05-21 14:37:00 +0900

    logback 라이브러리 추가 sysout 로직 logger로 개선

--- src/main/java/kr/or/ddit/servlet/TimesServlet.java
+++ src/main/java/kr/or/ddit/servlet/TimesServlet.java
@@ -8,7 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TimesServlet extends HttpServlet{
+	
+	//// logger를 쓰기 위해 선언 및 생성
+	//// Logger 임포트 시 slf4j를 선택해 줄 것
+	private Logger logger = LoggerFactory.getLogger(TimesServlet.class); // 맨 뒤 괄호는 현재 클래스명
+	//괄호 안은 kr.or.ddit.servlet.TimesServlet
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -19,9 +28,13 @@ public class TimesServlet extends HttpServlet{
 		
 		// String 타입 인수를 받아 String으로 반환
 		String param = req.getParameter("i"); // 얻어오는 파라미터값을 매칭
-		String param2 = req.getParameter("j");
 		
-//		System.out.println("param : " + param);
+		//// logger에서 사용하는 메서드. 바뀐다고 로그 출력 방법이 달라지는 것은 아니다
+		//// trace / debug / info / warn / error
+		logger.debug("param: {}, {}", param, "test");
+		
+		String param2 = req.getParameter("j");
+		logger.debug("param2: {}", param2);
 		
 		String g = "";
 		g += "<html>";
