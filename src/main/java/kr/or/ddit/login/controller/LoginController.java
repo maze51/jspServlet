f72747ffa5439c505ee7125415af8c829c0579d1
YC <mazexit@gmail.com>
2019-05-23 17:08:38 +0900

    로그인 프로세스 개선  - 정상 로그인한 사용자가 로그인 화면을 요청하면 main.jsp로 이동 페이지 모듈화 적용  jsp include 지시자 사용  header, left, basicLib 3개로 모듈화

--- src/main/java/kr/or/ddit/login/controller/LoginController.java
+++ src/main/java/kr/or/ddit/login/controller/LoginController.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.or.ddit.user.model.UserVo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,7 +51,15 @@ public class LoginController extends HttpServlet {
 		// 지금은 단순 login화면을 html로 응답을 생성해주는 작업이 필요
 		// /login/login.jsp로 위임 --> 서버상 별도의 상태 변경을 가하는 요청이 아니기 때문에
 		//							dispatch 방식으로 위임
-		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+		
+		// session에 사용자 정보가 있을 경우 --> main 화면으로 이동
+		if(request.getSession().getAttribute("USER_INFO")!=null){
+			request.getRequestDispatcher("/main.jsp").forward(request, response);
+		} else {
+			// session에 사용자 정보가 없을 경우 --> 기존 로직
+			request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+		}
+		
 	}
 	
 	// 로그인 요청 처리
@@ -67,6 +78,11 @@ public class LoginController extends HttpServlet {
 		
 		// 일치하면? 로그인 성공 : main 화면으로 이동
 		if(userId.equals("brown") && password.equals("brown1234")){
+			
+			// session에 사용자 정보를 넣어준다(사용빈도가 높기 때문에)
+			HttpSession session = request.getSession();
+			session.setAttribute("USER_INFO", new UserVo("브라운", "brown", "곰")); // 세션 이름에는 보통 대문자를 사용한다
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/main.jsp");
 			rd.forward(request, response);
 			
---
+++ src/main/java/kr/or/ddit/login/controller/LogoutController.java
@@ -0,0 +1,27 @@
+package kr.or.ddit.login.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		// 로그아웃 처리
+		session.invalidate();
+		
+		// 로그인으로 이동
+		request.getRequestDispatcher("/login").forward(request, response);
+		
+	}
+
+}
