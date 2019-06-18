f99890cd109099a610d994382cc82ecace35558b
YC <mazexit@gmail.com>
2019-06-18 11:15:36 +0900

    encoding 수정

--- src/main/java/kr/or/ddit/filter/RequestCountController.java
+++ src/main/java/kr/or/ddit/filter/RequestCountController.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RequestCountController
- */
 @WebServlet("/requestCount")
 public class RequestCountController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
